791e57f7daf520243aaef56d3fbef86af1767ddd
หนมจีบ <111548711+Oceannnx@users.noreply.github.com>
2022-10-28 09:20:40 +0700

    Update labexam.py

--- labexam.py
+++ labexam.py
@@ -1,3 +1,8 @@
-# namelist = ['kitty' , 'Hinton', 'Jack', 'Ted', 'Bahab', 'Cebul', 'David', 'Koller']
-# namelengthlist = (list((i,int(j)) for i in namelist for j in str(len(i))))
+namelist = ['kitty' , 'Hinton', 'Jack', 'Ted', 'Bahab', 'Cebul', 'David', 'Koller']
+namelengthlist = (list((i,int(j)) for i in namelist for j in str(len(i))))
 
+
+Dr_set = {'Jeffy', 'David', 'Ted'} 
+namelist = ['kitty' , 'Hinton', 'Jack', 'Ted', 'Bahab', 'Cebul', 'David', 'Koller']
+# print(list("Dr. " + i if i in Dr_set else i for i in namelist))
+titlednamelist = list("Dr. " + i if i in Dr_set else i for i in namelist)
